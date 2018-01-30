76cb89219ce1831849b7e54f75cc1b3de0e3c44f
liangyanxiang <liangyanxiang@juzix.io>
2018-01-30 18:17:38 +0800

    修改客户端宽度

--- src/main/index.js
+++ src/main/index.js
@@ -31,7 +31,7 @@ function createWindow() {
 	mainWindow = new BrowserWindow({
 		width: 1200,
 		height: 740,
-		minWidth: 1500,
+		minWidth: 1400,
 		minHeight: 600,
 		useContentSize: true,
 		frame: true, //是否创建有边框窗口
