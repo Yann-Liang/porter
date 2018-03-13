ac15a3aeca3a9f3966bd039e95e3d18f850ed59d
liangyanxiang <liangyanxiang@juzix.io>
2018-03-13 12:46:08 +0800

    update key-config

--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,5 +1,6 @@
-var nodemailer  = require('nodemailer');
-
+const nodemailer = require('nodemailer'),
+    config = require('@/config/key-config.json'),
+    ;
 class EmailService {
     constructor() {
         this.email= nodemailer.createTransport({
@@ -7,8 +8,8 @@ class EmailService {
             secureConnection: true, // 使用SSL方式（安全方式，防止被窃取信息）
             port:465,
             auth : {
-                user : '17154838705@163.com',
-                pass : '',
+                user : config.email.user,
+                pass : config.email.password,
             },
         });
     }
@@ -16,7 +17,7 @@ class EmailService {
         console.log('send email', subject, text, html);
         return new Promise((resolve, reject) => {
             this.email.sendMail({
-                from: `17154838705@163.com`,//'"你的名字" <你的邮箱地址>',
+                from: config.email.user,//'"你的名字" <你的邮箱地址>',
                 to: `liangyanxiangde@vip.qq.com`,//'"用户1" <邮箱地址1>, "用户2" <邮箱地址2>',
                 cc: '',  //抄送
                 bcc: '',    //密送
