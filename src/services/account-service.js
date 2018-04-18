a3862e92b9f151a85ee50760ed9f45aa1f93bb3c
liangyanxiang <liangyanxiang@juzix.io>
2018-04-18 18:26:27 +0800

    deve

--- src/services/account-service.js
+++ src/services/account-service.js
@@ -1,14 +1,28 @@
-const moment = require('moment')
+const moment = require('moment'),
+    fs = require('fs')
     ;
 
+let savePath = ``;
+
 class AccountService {
-    constructor() {
-        this.balance = {};
+    constructor(type) {
+        this.type = type;//交易所类型
+        this.balance = {};//账户财务
+        this.accounts = {};//账户列表
+        this.balanceHistory = {
+            balance: null,
+            recordedTime: ''//上次记录时间
+        };
+        savePath = `./static/json/${this.type}-balance.json`
+        //加载旧数据
+        fs.readFile(savePath, 'utf8', (err, data) => {
+            if (err) throw err;
+            this.balanceHistory = JSON.parse(data);
+        });
 
     }
-
-
-    removeEmptyData(exchangeName, list) {
+    //
+    removeEmptyData(list) {
         return new Promise((resolve, reject) => {
             if (list && list.length) {
                 const len = list.length;
@@ -16,11 +30,11 @@ class AccountService {
                 for (let i = 0, item; i < len; i++) {
                     item = list[i];
                     if (item.balance > 0) {
-                        typeof balance[item.currency] == "undefined" ? balance[item.currency] = {} : '';
+                        typeof balance[item.currency] == "undefined" ? balance[item.currency] = { trade: 0, frozen:0} : '';
                         balance[item.currency][item.type] = item.balance;
                     }
                 }
-                this.balance[exchangeName] = balance;
+                this.balance = balance;
                 resolve(balance);
             } else {
                 reject();
@@ -28,61 +42,71 @@ class AccountService {
         });
     }
 
-    getAccountBalanceContent(exchangeName) {
+    getAccountBalanceContent() {
+        const list = this.balance,
+            { balance,recordedTime } = this.balanceHistory,//上次财务情况
+            time = moment().format("YYYY年MM月DD日HH:mm"),
+            subject = `[财务报告]${time}${this.type}账户余额`;
 
-        const list = this.balance[exchangeName];
-        let oldList = null,
-        balance={};
-
-        if (localStorage.balance && localStorage.balance[exchangeName]) {
-            oldList = localStorage.balance[exchangeName];
-        }
-
-        if (localStorage.balance) {
-            balance  = JSON.parse(localStorage.balance);
-            oldList = balance[exchangeName] ? balance[exchangeName] : {};
-        }
+        let html = [`<table border="1" style="text-align:center;border-collapse:collapse;">
+                        <caption>${subject}</caption>
+                        <thead>
+                             <tr>
+                                <th>序号</th>
+                                <th>币种</th>
+                                <th>可用</th>
+                                <th>冻结</th>
+                                <th>上次可用</th>
+                                <th>上次冻结</th>
+                                <th>总计</th>
+                                <th>${recordedTime}总计</th>
+                                <th>差额</th>
+                            </tr>
+                        </thead>
+                        <tbody>`
+        ],
+            i = 0,
+            oldFrozen,
+            oldTrade,
+            oldTotal;
 
         return new Promise((resolve, reject) => {
-
-            const time = moment().format("YYYY年MM月DD日HH:mm"),
-                subject = `[财务报告]${time} ${exchangeName}账户余额`;
-            let html = [`<table border="1" style="text-align:center;border-collapse:collapse;">
-                            <caption>${subject}</caption>
-                            <thead>
-                                <tr>
-                                    <th>币种</th>
-                                    <th>可用</th>
-                                    <th>冻结</th>
-                                    <th>总计</th>
-                                    <th>上次总计</th>
-                                    <th>差额</th>
-                                </tr>
-                            </thead>
-                            <tbody>`
-            ];
             for (const key in list) {
                 const item = list[key],
                     trade = item.trade ? Number(item.trade) : 0,
                     frozen = item.frozen ? Number(item.frozen) : 0,
-                    total = trade + frozen,
-                    oldTotal = oldList[key] ? oldList[key].total : 0;
+                    total = trade + frozen
+                    ;
+
+                if (balance[key]) {
+                    oldFrozen = balance[key].frozen;
+                    oldTrade = balance[key].trade;
+                    oldTotal = balance[key].total;
+                }
 
                 list[key].total = total;
 
                 html.push(`<tr>
+                            <td>${++i}</td>
                             <td>${key}</td>
                             <td>${trade}</td>
                             <td>${frozen}</td>
+                            <td>${oldTrade}</td>
+                            <td>${oldFrozen}</td>
                             <td>${total}</td>
                             <td>${oldTotal}</td>
                             <td>${total - oldTotal}</td>
                         </tr>`);
             }
 
-            balance[exchangeName] = list;
-            localStorage.balance = JSON.stringify(balance);
-            localStorage[`${exchangeName}RecordedTime`] = time;
+            this.balanceHistory = {
+                balance: list,
+                recordedTime: time,
+            };
+            fs.writeFile(savePath, JSON.stringify(this.balanceHistory), (err) => {
+                if (err) throw err;
+                console.log('The file has been saved!');
+            });
             html.push('</tbody></table>');
             html = html.join('');
             resolve({ subject, html });
@@ -94,4 +118,4 @@ class AccountService {
 };
 
 //导出一个对象
-export default new AccountService;
+module.exports = AccountService;
--- src/services/http/huobi.js
+++ src/services/http/huobi.js
@@ -1,94 +1,154 @@
-import Http from 'axios'
-import API from '@/config/API-config'
-
-const config = require('@/config/key-config.json'),
+const request = require('request'),
+    API = require('../../../config/API-config'),
+    config = require('../../../config/default.json'),
     CryptoJS = require('crypto-js'),
     moment = require('moment'),
     HmacSHA256 = require('crypto-js/hmac-sha256')
 
     ;
-const interceptorsOfReq = () => {
-    return Http.interceptors.request.use(
-        config => {
-            console.log('请求URL== ' + config.url, '\n请求参数==', config.data);
-            return config;
-        },
-        err => {
-            return Promise.reject(err);
-        });
+const sign_sha = (method, baseurl, path, data) => {
+    var pars = [];
+    for (let item in data) {
+        pars.push(item + "=" + encodeURIComponent(data[item]));
+    }
+    var p = pars.sort().join("&");
+    var meta = [method, baseurl, path, p].join('\n');
+    //console.log('meta',meta);
+    var hash = HmacSHA256(meta, config.huobi.secretkey);
+    var Signature = encodeURIComponent(CryptoJS.enc.Base64.stringify(hash));
+    // console.log(`Signature: ${Signature}`);
+    p += `&Signature=${Signature}`;
+    // console.log(p);
+    return p;
 },
-    interceptorsOfRes = () => {
-        Http.interceptors.response.use(function (response) {
-            console.log(response.config.url + '的响应数据↓↓↓\n', response.data);
-            if (response.data.errorCode == 4) {
-                localStorage.removeItem('sessionid');
-                localStorage.removeItem('user');
-                vueVm.loginPopFlag = true;
-            }
-
-            if (typeof response.data.data == null) {
-                console.log('没有查询到数据')
-            };
-
-            return response;
-        }, function (error) {
-            console.log("请求失败");
-            return Promise.reject(error);
-        });
-    },
-    sign_sha=(method, baseurl, path, data)=> {
-        var pars = [];
-        for (let item in data) {
-            pars.push(item + "=" + encodeURIComponent(data[item]));
-        }
-        var p = pars.sort().join("&");
-        var meta = [method, baseurl, path, p].join('\n');
-        //console.log('meta',meta);
-        var hash = HmacSHA256(meta, config.huobi.secretkey);
-        var Signature = encodeURIComponent(CryptoJS.enc.Base64.stringify(hash));
-        // console.log(`Signature: ${Signature}`);
-        p += `&Signature=${Signature}`;
-        // console.log(p);
-        return p;
-    },
-    get_body=()=> {
+    get_body = () => {
         return {
             AccessKeyId: config.huobi.access_key,
             SignatureMethod: "HmacSHA256",
             SignatureVersion: 2,
             Timestamp: moment.utc().format('YYYY-MM-DDTHH:mm:ss'),
         };
+    },
+    get_auth = () => {
+        var sign = config.huobi.trade_password + 'hello, moto';
+        var md5 = CryptoJS.MD5(sign).toString().toLowerCase();
+        let ret = encodeURIComponent(JSON.stringify({
+            assetPwd: md5
+        }));
+        return ret;
     }
     ;
-Http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'//"application/json;charset=utf-8";
-Http.defaults.headers.post["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36";
 
 //请求类
 class huobiHttpService {
     constructor() {
 
         this.getAccount = this.get.bind(this, API.HUO_BI.accounts);
+        this.accountBalance = this.get.bind(this, API.HUO_BI.accountBalance);
+
         this.klineHistory = this.get.bind(this, API.HUO_BI.klineHistory);
         this.tradeHistory = this.get.bind(this, API.HUO_BI.tradeHistory);
-        this.accountBalance = this.get.bind(this, API.HUO_BI.accountBalance);
+
         this.querySymbols = this.get.bind(this, API.HUO_BI.querySymbols);
-        // interceptorsOfReq();
-        // interceptorsOfRes();
+
+        this.getOreders = this.get.bind(this, API.HUO_BI.orders);
+        this.matchresults = this.get.bind(this, API.HUO_BI.matchresults);
+        this.subOrder = this.post.bind(this, API.HUO_BI.subOrder);
+    }
+
+    // matchresults(orderId) {
+    //     const url = orderId ? `${API.HUO_BI.orders}/${orderId}/matchresults` : `${API.HUO_BI.orders}/matchresults`;
+    //     return new Promise((resolve, reject) => {
+    //         this.get(url).then(resolve).catch(reject);
+    //     });
+    // }
+    /**
+     * @description 查询某个订单的成交明细
+     * @param orderId 订单ID
+     * @memberof huobiHttpService
+     */
+    orderInfo(orderId) {
+
     }
 
     get(url, params = {}) {
         let body = get_body();
+
         for (const key in params) {
             body[key] = params[key];
         }
-        let payload = sign_sha('GET', `api.huobipro.com`, url, body);
-        return Http.get(`${API.HUO_BI_BASE}${url}?${payload}`).then(res => res.data);
+
+        let payload = sign_sha('GET', `api.huobipro.com`, url, body),
+            httpOptions = {
+                url: `${API.HUO_BI_BASE}${url}?${payload}`,
+                method: 'get',
+                timeout: 3000,
+                headers: {
+                    'content-type': 'application/json;charset=utf-8',
+                    'Accept-Language': 'zh-cn'
+                },
+                proxy: '',
+                agentOptions: {
+                    maxSockets: 256,
+                }
+            };
+
+        return new Promise((resolve, reject) => {
+            request(httpOptions, function (err, res, body) {
+                if (err) {
+                    reject(err);
+                } else {
+                    if (res.statusCode == 200) {
+                        resolve(JSON.parse(body));
+                    } else {
+                        reject(res.statusCode);
+                    }
+                }
+            }).on('error', console.error);
+        });
     }
 
     post(url, params = {}) {
-        return Http.post(url, params).then(res => res.data);
+        let body = get_body(),
+            payload = sign_sha('POST', `api.huobipro.com`, url, body);
+
+        for (const key in params) {
+            body[key] = params[key];
+        }
+
+        let httpOptions = {
+            url: `${API.HUO_BI_BASE}${url}?${payload}`,
+            method: 'post',
+            body: JSON.stringify(body),
+            timeout: 3000,
+            headers: {
+                'content-type': 'application/json;charset=utf-8',
+                "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36",
+                'Accept-Language': 'zh-cn',
+                'AuthData': get_auth(),
+            },
+            proxy: '',
+            agentOptions: {
+                maxSockets: 256,
+            }
+        };
+
+        return new Promise((resolve, reject) => {
+            request(httpOptions, function (err, res, body) {
+                if (err) {
+                    reject(err);
+                } else {
+                    if (res.statusCode == 200) {
+                        resolve(JSON.parse(body));
+                    } else {
+                        reject(res.statusCode);
+                    }
+                }
+            }).on('error', console.error);
+        });
     }
+
 };
 
 //导出一个对象
-export default new huobiHttpService();
+module.exports = new huobiHttpService();
