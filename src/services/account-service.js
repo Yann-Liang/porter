41cf3ac3be3513891a0fd4885f83a975784a09be
liangyanxiang <liangyanxiang@juzix.io>
2018-05-02 13:44:23 +0800

    deve

--- src/services/account-service.js
+++ src/services/account-service.js
@@ -13,11 +13,15 @@ class AccountService {
             balance: null,
             recordedTime: ''//上次记录时间
         };
-        savePath = `./static/json/${this.type}-balance.json`
+        savePath = `./static/exchange/${this.type}/balance/balance.json`
         //加载旧数据
         fs.readFile(savePath, 'utf8', (err, data) => {
-            if (err) throw err;
-            this.balanceHistory = JSON.parse(data);
+            if (err) {
+                console.warn(err);
+            } else {
+                data ? this.balanceHistory = JSON.parse(data) : '';
+            };
+
         });
 
     }
@@ -30,7 +34,7 @@ class AccountService {
                 for (let i = 0, item; i < len; i++) {
                     item = list[i];
                     if (item.balance > 0) {
-                        typeof balance[item.currency] == "undefined" ? balance[item.currency] = { trade: 0, frozen:0} : '';
+                        typeof balance[item.currency] == "undefined" ? balance[item.currency] = { trade: 0, frozen: 0 } : '';
                         balance[item.currency][item.type] = item.balance;
                     }
                 }
@@ -41,10 +45,33 @@ class AccountService {
             }
         });
     }
+    /**
+     * @description 格式化
+     *  将{balance:'余额',currency:'币种',type:'类型 trade: 交易余额，frozen: 冻结余额'}
+     *  改为 currency{trade,frozen}
+     *
+     * @param {Array} list
+     * @returns
+     * @memberof AccountService
+     */
+    formatBalance(list) {
+        if (list && list.length) {
+            const len = list.length;
+            let balance = {};
+            for (let i = 0, item; i < len; i++) {
+                item = list[i];
+                typeof balance[item.currency] == "undefined" ? balance[item.currency] = { trade: 0, frozen: 0 } : '';
+                balance[item.currency][item.type] = item.balance;
+            }
+            this.balance = balance;
+            return balance;
+        }
+        return null;
+    }
 
     getAccountBalanceContent() {
         const list = this.balance,
-            { balance,recordedTime } = this.balanceHistory,//上次财务情况
+            { balance, recordedTime } = this.balanceHistory,//上次财务情况
             time = moment().format("YYYY年MM月DD日HH:mm"),
             subject = `[财务报告]${time}${this.type}账户余额`;
 
@@ -70,23 +97,23 @@ class AccountService {
             oldTrade,
             oldTotal;
 
-        return new Promise((resolve, reject) => {
-            for (const key in list) {
-                const item = list[key],
-                    trade = item.trade ? Number(item.trade) : 0,
-                    frozen = item.frozen ? Number(item.frozen) : 0,
-                    total = trade + frozen
-                    ;
 
-                if (balance[key]) {
-                    oldFrozen = balance[key].frozen;
-                    oldTrade = balance[key].trade;
-                    oldTotal = balance[key].total;
-                }
+        for (const key in list) {
+            const item = list[key],
+                trade = item.trade ? Number(item.trade) : 0,
+                frozen = item.frozen ? Number(item.frozen) : 0,
+                total = trade + frozen
+                ;
 
-                list[key].total = total;
+            if (balance&&balance[key]) {
+                oldFrozen = balance[key].frozen;
+                oldTrade = balance[key].trade;
+                oldTotal = balance[key].total;
+            }
 
-                html.push(`<tr>
+            list[key].total = total;
+
+            html.push(`<tr>
                             <td>${++i}</td>
                             <td>${key}</td>
                             <td>${trade}</td>
@@ -97,23 +124,38 @@ class AccountService {
                             <td>${oldTotal}</td>
                             <td>${total - oldTotal}</td>
                         </tr>`);
-            }
+        }
 
-            this.balanceHistory = {
-                balance: list,
-                recordedTime: time,
-            };
-            fs.writeFile(savePath, JSON.stringify(this.balanceHistory), (err) => {
-                if (err) throw err;
-                console.log('The file has been saved!');
-            });
-            html.push('</tbody></table>');
-            html = html.join('');
-            resolve({ subject, html });
+        this.balanceHistory = {
+            balance: list,
+            recordedTime: time,
+        };
+        this.saveBalance(this.balanceHistory);
+        html.push('</tbody></table>');
+        html = html.join('');
 
-        });
+        return { subject, html };
 
     }
+    /**
+     * @description 将财务信息保存到磁盘
+     *
+     * @param {object} data
+     * @memberof AccountService
+     */
+    saveBalance(data) {
+        const STR = JSON.stringify(data);
+        fs.writeFile(savePath, STR, (err) => {
+            if (err) throw err;
+            console.log(`The ${savePath} has been saved!`);
+        });
+
+        fs.writeFile(`./static/exchange/${this.type}/balance/${moment().format('YYYY-MM-DD HH')}.json`, STR, (err) => {
+            if (err) throw err;
+            console.log('The dateFile has been saved!');
+        });
+    }
+
 
 };
 
